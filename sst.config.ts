e4c5a026da963bd207972369bbf51ea2b4a02f7e
DavideSelvaggio <selvaggiodavide@hotmail.it>
2025-05-28 14:05:31 +0800

    Update SST configuration to conditionally set providers and domain based on the deployment stage

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="./.sst/platform/config.d.ts" />
+
 export default $config({
   app(input) {
     return {
@@ -7,15 +8,15 @@ export default $config({
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
-      providers: { cloudflare: "6.2.0" },
+      providers: input?.stage === "production" ? { cloudflare: "6.2.0" } : { aws: true },
     };
   },
   async run() {
+    const domain = $app.stage === "production" 
+      ? { name: "docs.hebo.ai", dns: sst.cloudflare.dns() }
+      : undefined;
     new sst.aws.Nextjs("HeboDocs", {
-      domain: {
-        name: "docs.hebo.ai",
-        dns: sst.cloudflare.dns(),
-      },
+      domain: domain,
     });
   },
 });
